ebdb672888882c847b0b987cc2d49a9d220f55fd
earica <74029982+earica@users.noreply.github.com>
2021-04-27 21:35:03 -0500

    Created project

---
+++ org/launchcode/Go/Forage/GoForageApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Go.Forage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoForageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoForageApplication.class, args);
+	}
+
+}
